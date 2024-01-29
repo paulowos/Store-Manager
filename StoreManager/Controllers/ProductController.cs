b67a2caae24806e3076d5ee666f44e616342a6ee
Paulo Willian <paulowos@gmail.com>
2024-01-29 11:39:24 -0300

    feat: ProductController.cs documentation

--- StoreManager/Controllers/ProductController.cs
+++ StoreManager/Controllers/ProductController.cs
@@ -7,6 +7,7 @@ namespace StoreManager.Controllers;
 
 [ApiController]
 [Route("[controller]")]
+[Produces("application/json")]
 public class ProductController : ControllerBase
 {
     private readonly IProductRepository _repository;
@@ -16,12 +17,24 @@ public class ProductController : ControllerBase
         _repository = repository;
     }
 
+    /// <summary>
+    ///     Get all Products
+    /// </summary>
+    /// <response code="200">Returns all Products</response>
     [HttpGet]
     public ActionResult<IEnumerable<Product>> GetAll()
     {
         return Ok(_repository.GetAll());
     }
 
+    /// <summary>
+    ///     Get Product by Id
+    /// </summary>
+    /// <param name="id">Product Id</param>
+    /// <response code="200">Returns a Product</response>
+    /// <response code="404">Product not found</response>
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorMessage), StatusCodes.Status404NotFound)]
     [HttpGet("{id:int}")]
     public ActionResult<Product> GetById(int id)
     {
@@ -31,6 +44,14 @@ public class ProductController : ControllerBase
         return Ok(product);
     }
 
+    /// <summary>
+    ///     Add a new Product
+    /// </summary>
+    /// <param name="productDto"> Product Name</param>
+    /// <response code="201"> Returns the created Product </response>
+    /// <response code="400"> Invalid Product Name </response>
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [HttpPost]
     public ActionResult<Product> Add(ProductDto productDto)
     {
@@ -38,6 +59,17 @@ public class ProductController : ControllerBase
         return CreatedAtAction(nameof(GetById), new { id = product.Id }, product);
     }
 
+    /// <summary>
+    ///     Update a Product
+    /// </summary>
+    /// <param name="id">Product Id</param>
+    /// <param name="productDto">Product Name</param>
+    /// <response code="200"> Returns the updated Product </response>
+    /// <response code="404"> Product not found </response>
+    /// <response code="400"> Invalid Product Name </response>
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ErrorMessage), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [HttpPut("{id:int}")]
     public ActionResult<Product> Update([FromRoute] int id, [FromBody] ProductDto productDto)
     {
@@ -52,6 +84,14 @@ public class ProductController : ControllerBase
         }
     }
 
+    /// <summary>
+    ///     Delete a Product
+    /// </summary>
+    /// <param name="id">Product Id</param>
+    /// <response code="204"> Product deleted </response>
+    /// <response code="404"> Product not found </response>
+    [ProducesResponseType(StatusCodes.Status204NoContent)]
+    [ProducesResponseType(typeof(ErrorMessage), StatusCodes.Status404NotFound)]
     [HttpDelete("{id:int}")]
     public ActionResult Delete(int id)
     {
@@ -66,6 +106,12 @@ public class ProductController : ControllerBase
         }
     }
 
+    /// <summary>
+    ///     Search Products by Name
+    /// </summary>
+    /// <param name="q">Product Name</param>
+    /// <response code="200"> Returns Products </response>
+    [ProducesResponseType(StatusCodes.Status200OK)]
     [HttpGet("search")]
     public ActionResult<IEnumerable<Product>> Search([FromQuery] string? q = "")
     {
